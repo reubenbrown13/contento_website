ad90f20b59f8ba07b65ab83be760eb80d5bc3a7b
und0ck3d <und0ck3d@protonmail.com>
2017-10-06 19:24:20 +0100

    removed unecessary tasks

--- lib/mix/tasks/contento.build.assets.ex
+++
@@ -1,9 +0,0 @@
-defmodule Mix.Tasks.Contento.Build.Assets do
-  use Mix.Task
-
-  def run(_args) do
-    Mix.Shell.cmd("cd assets && yarn install && yarn build", fn output ->
-      Mix.Shell.IO.info(output)
-    end)
-  end
-end
