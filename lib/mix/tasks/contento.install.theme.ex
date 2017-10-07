0a713d98e3eee2b5376d786407628e9fffc75448
und0ck3d <und0ck3d@protonmail.com>
2017-10-07 18:05:12 +0100

    added option to skip downloading (or copying)

--- lib/mix/tasks/contento.install.theme.ex
+++ lib/mix/tasks/contento.install.theme.ex
@@ -11,22 +11,34 @@ defmodule Mix.Tasks.Contento.Install.Theme do
   @config_filename "theme.json"
 
   def run(args) do
+    options = parse_args(args)
+
     ensure_started(Contento.Repo, [])
 
-    repo_or_dir = Enum.at(args, 0)
-
-    if "--local" in args do
-      {:ok, _} = copy_theme(repo_or_dir)
+    if options[:local] do
+      if !options[:skip_download] do
+        {:ok, _} = copy_theme(options[:target])
+      end
     else
-      {:ok, _} = clone_theme(repo_or_dir)
+      if !options[:skip_download] do
+        {:ok, _} = clone_theme(options[:target])
+      end
     end
 
-    with {:ok, config} <- theme_config(repo_or_dir),
-         :ok <- copy_assets(repo_or_dir),
-         {:ok, theme} <- Themes.create_theme(config) do
+    with {:ok, config} <- theme_config(options[:target]),
+         :ok <- copy_assets(options[:target]),
+         {:ok, theme} <- create_or_update_theme(config) do
       Logger.info("Theme #{theme.name} installed successfully!")
     else
       err -> Mix.raise("#{inspect err}")
+    end
+  end
+
+  defp create_or_update_theme(theme_params) do
+    if theme = Themes.get_theme(alias: theme_params["alias"]) do
+      Themes.update_theme(theme, theme_params)
+    else
+      Themes.create_theme(theme_params)
     end
   end
 
@@ -48,14 +60,14 @@ defmodule Mix.Tasks.Contento.Install.Theme do
     |> Task.await()
   end
 
-  defp theme_config(repo) do
-    path = Path.join([@base_path, theme_alias(repo), @config_filename])
+  defp theme_config(target) do
+    path = Path.join([@base_path, theme_alias(target), @config_filename])
 
     with {:ok, config} <- File.read(path) do
       config = config
       |> Poison.decode!()
       |> Map.merge(%{
-        "alias" => theme_alias(repo)
+        "alias" => theme_alias(target)
       })
 
       {:ok, config}
@@ -85,5 +97,14 @@ defmodule Mix.Tasks.Contento.Install.Theme do
       end
     end)
     |> Task.await(:infinity)
+  end
+
+  defp parse_args(argv) do
+    {flags, params} = OptionParser.parse!(argv, strict: [
+      local: :boolean,
+      skip_download: :boolean
+    ])
+
+    flags ++ [target: Enum.at(params, 0)]
   end
 end
